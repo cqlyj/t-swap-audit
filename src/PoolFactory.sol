d18615a14ccf7dd1a33e8c539337456012cf8027
cqlyj <luoyingjie0721@gmail.com>
2024-10-11 17:22:46 +0800

    recon on PoolFactory contract

--- src/PoolFactory.sol
+++ src/PoolFactory.sol
@@ -14,11 +14,13 @@
 // SPDX-License-Identifier: GNU General Public License v3.0
 pragma solidity 0.8.20;
 
-import { TSwapPool } from "./TSwapPool.sol";
-import { IERC20 } from "forge-std/interfaces/IERC20.sol";
+import {TSwapPool} from "./TSwapPool.sol";
+import {IERC20} from "forge-std/interfaces/IERC20.sol";
 
 contract PoolFactory {
     error PoolFactory__PoolAlreadyExists(address tokenAddress);
+
+    // @audit-info this error is not used
     error PoolFactory__PoolDoesNotExist(address tokenAddress);
 
     /*//////////////////////////////////////////////////////////////
@@ -38,6 +40,7 @@ contract PoolFactory {
                                FUNCTIONS
     //////////////////////////////////////////////////////////////*/
     constructor(address wethToken) {
+        // @audit-info lack zero address check
         i_wethToken = wethToken;
     }
 
@@ -48,9 +51,23 @@ contract PoolFactory {
         if (s_pools[tokenAddress] != address(0)) {
             revert PoolFactory__PoolAlreadyExists(tokenAddress);
         }
-        string memory liquidityTokenName = string.concat("T-Swap ", IERC20(tokenAddress).name());
-        string memory liquidityTokenSymbol = string.concat("ts", IERC20(tokenAddress).name());
-        TSwapPool tPool = new TSwapPool(tokenAddress, i_wethToken, liquidityTokenName, liquidityTokenSymbol);
+
+        // q what if the name function reverts? Wired ERC20 tokens
+        string memory liquidityTokenName = string.concat(
+            "T-Swap ",
+            IERC20(tokenAddress).name()
+        );
+        // @audit-info this should be .symbol()
+        string memory liquidityTokenSymbol = string.concat(
+            "ts",
+            IERC20(tokenAddress).name()
+        );
+        TSwapPool tPool = new TSwapPool(
+            tokenAddress,
+            i_wethToken,
+            liquidityTokenName,
+            liquidityTokenSymbol
+        );
         s_pools[tokenAddress] = address(tPool);
         s_tokens[address(tPool)] = tokenAddress;
         emit PoolCreated(tokenAddress, address(tPool));
